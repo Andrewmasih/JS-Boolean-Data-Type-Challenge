91a07b1a285888759cff7a4bc9a71b735cc1d8ab
Andrew Masih <armasih@outlook.com>
2021-06-08 22:40:28 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+/* its essential to understamd different data types, and knowing which ones to work with.. */
+
+/* challenge:
+
+1/ create a varible, stringTrue, which has a value of TRUE and is a string.
+
+2/ create a second varible, booleanTrue, whcih has a value of TRUE,but is a boolean data types
+
+requirements:
+1/ the varaible must be named correctly and have the corrrect data types */
+
+let stringTrue = "true";
+let booleanTrue = true;
+
+console.log(stringTrue);
+console.log(booleanTrue);
